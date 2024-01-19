3b77c38bf75b338769a43bdb356e1bb4ca78c851
Niklas P <contact@niklasplessing.net>
2024-01-19 09:44:38 +0100

    initial commit

--- handlers/handleOldMessagesAndReactions.ts
+++ handlers/handleOldMessagesAndReactions.ts
@@ -1,34 +1,57 @@
-import { discordClient } from "@/client";
+import { discordClient, logger } from "@/client";
 import * as config from "../config";
 import { TextChannel } from "discord.js";
+import { delay } from "./util";
 
 export const handleOldMessagesAndReactions = async (limit: number) => {
-  console.log("handleOldMessagesAndReactions");
+  const delayBetweenMessages = 500; // Delay in milliseconds (e.g., 500ms = 0.5 seconds)
+  const delayBetweenChannels = 5000; // Delay in milliseconds (e.g., 5000ms = 5 seconds)
 
-  const channel = (await discordClient.channels.fetch(
-    config.CHANNELS_TO_MONITOR[0]
-  )) as TextChannel;
+  let channelCount = 0;
+  let messageCount = 0;
+  let reactionCount = 0;
 
-  console.log("channel", channel?.guild?.name);
+  try {
+    for (const channelId of config.CHANNELS_TO_MONITOR) {
+      const channel = (await discordClient.channels.fetch(
+        channelId
+      )) as TextChannel;
+      logger.log(
+        `Looking for old messages and reactions in channel: #${channel?.guild?.name} - ${channel?.name}`
+      );
 
-  if (channel instanceof TextChannel) {
-    const messages = await channel.messages.fetch({ limit }); // Adjust limit as needed
+      if (channel) {
+        channelCount++;
+        const messages = await channel.messages.fetch({ limit }); // Adjust limit as needed
 
-    for (const message of messages.values()) {
-      await message.fetch();
-      console.log("message", message.content);
+        for (const message of messages.values()) {
+          await message.fetch();
+          messageCount++;
 
-      // Fetching all reactions for each message
-      for (const reaction of message.reactions.cache.values()) {
-        await reaction.fetch(); // Fetch all users who reacted with this emoji
-        console.log("reaction", reaction.emoji.name);
-        const users = await reaction.users.fetch();
-        for (const user of users.values()) {
-          console.log(
-            `User ${user.tag} reacted with ${reaction.emoji.name} on message: ${message.content}`
-          );
+          // Fetching all reactions for each message
+          for (const reaction of message.reactions.cache.values()) {
+            await reaction.fetch(); // Fetch all users who reacted with this emoji
+            reactionCount++;
+
+            const users = await reaction.users.fetch();
+            for (const user of users.values()) {
+              logger.log(
+                `User ${user.tag} reacted with ${reaction.emoji.name} on message: ${message.id}`
+              );
+            }
+          }
+
+          await delay(delayBetweenMessages);
         }
       }
+
+      await delay(delayBetweenChannels);
     }
+  } catch (error) {
+    logger.error("Error fetching old messages and reactions:", error);
   }
+
+  logger.log(
+    `Fetched ${messageCount} old messages and ${reactionCount} reactions from ${channelCount} channels.`
+  );
 };
