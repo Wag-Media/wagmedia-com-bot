22e1df4c21020b398c6fbbf41657046066bd148e
Niklas P <contact@niklasplessing.net>
2024-02-13 14:30:56 +0100

    connection handling

--- index.ts
+++ index.ts
@@ -24,8 +24,6 @@ discordClient.on(Events.ShardReconnecting, () => {
 });
 
 discordClient.on(Events.ClientReady, () => {
-  console.log("Bot is ready", config.GUILD_ID, config.CATEGORIES_TO_MONITOR);
-
   if (!config.GUILD_ID) {
     logger.error(
       "GUILD_ID is not set in the environment variables. Please set it and try again."
@@ -33,19 +31,27 @@ discordClient.on(Events.ClientReady, () => {
     process.exit(1);
   }
 
-  console.log(discordClient.guilds.cache);
+  let onCorrectGuild = false;
 
   // Iterate over all guilds the bot is part of
   discordClient.guilds.cache.forEach(async (guild) => {
     if (guild.id !== config.GUILD_ID.toString()) {
       logger.warn(
-        `Guild is not configured as the guild the bot should work with. Leaving guild: ${guild.name}`
+        `Guild is not configured as the guild the bot should work with, guild: ${guild.name}`
       );
-      guild.leave();
+      // guild.leave();
+    } else {
+      logIntroMessage(guild, discordClient);
+      onCorrectGuild = true;
     }
-
-    logIntroMessage(guild, discordClient);
   });
+
+  if (!onCorrectGuild) {
+    logger.error(
+      `The bot is not part of the guild with the ID ${config.GUILD_ID}. Please add the bot to the correct guild and try again.`
+    );
+    process.exit(1);
+  }
 });
 
 discordClient.on(Events.MessageCreate, async (message) => {
