9959015ea4fe42bc8af07e6c473c7f2a4063e6a9
Niklas P <contact@niklasplessing.net>
2024-04-18 01:58:11 +0200

    force fetch user before saving

--- data/user.ts
+++ data/user.ts
@@ -33,25 +33,27 @@ export const findOrCreateUser = async (
 };
 
 export const findOrCreateUserFromDiscordUser = async (user: DiscordUser) => {
+  const fullUser = await user.fetch(true);
+
   const dbUser = await prisma.user.upsert({
     where: {
-      discordId: user.id,
+      discordId: fullUser.id,
     },
     update: {
-      // Update the avatar every time the user posts something
-      avatar: user?.displayAvatarURL(),
-      avatarDecoration: user.avatarDecorationURL(),
-      banner: user?.bannerURL(),
-      accentColor: user?.hexAccentColor,
-      name: user?.displayName,
+      // Update the avatar every time the fullUser posts something
+      avatar: fullUser?.displayAvatarURL(),
+      avatarDecoration: fullUser.avatarDecorationURL(),
+      banner: fullUser?.bannerURL(),
+      accentColor: fullUser?.hexAccentColor,
+      name: fullUser?.displayName,
     },
     create: {
-      discordId: user.id,
-      avatar: user?.displayAvatarURL(),
-      avatarDecoration: user.avatarDecorationURL(),
-      banner: user?.bannerURL(),
-      accentColor: user?.hexAccentColor,
-      name: user?.displayName,
+      discordId: fullUser.id,
+      avatar: fullUser?.displayAvatarURL(),
+      avatarDecoration: fullUser.avatarDecorationURL(),
+      banner: fullUser?.bannerURL(),
+      accentColor: fullUser?.hexAccentColor,
+      name: fullUser?.displayName,
     },
   });
   return dbUser;
