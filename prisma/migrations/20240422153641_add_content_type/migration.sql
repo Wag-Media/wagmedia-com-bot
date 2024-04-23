3519550a39fa1de9bc8c57ba538d1a99101374e6
Niklas P <contact@niklasplessing.net>
2024-04-23 09:09:06 +0200

    rollback migration2

--- prisma/migrations/20240422153641_add_content_type/migration.sql
+++ prisma/migrations/20240422153641_add_content_type/migration.sql
@@ -3,10 +3,3 @@ CREATE TYPE "ContentType" AS ENUM ('news', 'article');
 
 -- AlterTable
 ALTER TABLE "Post" ADD COLUMN     "contentType" "ContentType";
-
-UPDATE "Post"
-SET "contentType" = CASE
-  WHEN "discordLink" LIKE '%960677080784330793%' OR "discordLink" LIKE '%985566609689169920%' THEN 'article'
-  WHEN "discordLink" LIKE '%919836627608690699%' THEN 'news'
-  ELSE NULL
-END;
