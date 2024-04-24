33e2506f14dedca181de5db943755b5666f88c7e
Niklas P <contact@niklasplessing.net>
2024-04-24 03:42:09 +0200

    wip staging

---
+++ prisma/log-multiple-post-earnings.ts
@@ -0,0 +1,26 @@
+const { PrismaClient } = require("@prisma/client");
+
+const prisma = new PrismaClient();
+
+main()
+  .catch(async (e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => await prisma.$disconnect());
+
+async function main() {
+  // get all posts with multiple contentEarnings
+  const posts = await prisma.$queryRaw`
+  SELECT p.*
+  FROM "Post" AS p
+  JOIN (
+    SELECT "postId", COUNT(*) AS "earningsCount"
+    FROM "ContentEarnings"
+    GROUP BY "postId"
+    HAVING COUNT(*) > 1
+  ) AS earnings ON earnings."postId" = p."id"
+`;
+
+  console.log("Posts with multiple contentEarnings:", posts);
+}
