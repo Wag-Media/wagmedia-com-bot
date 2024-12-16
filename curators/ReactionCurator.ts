2612093b3bef5da45cedd6c2a87e89b49a2082db
Niklas P <contact@niklasplessing.net>
2024-12-16 16:25:48 +0100

    fix discrepancy resolver

--- curators/ReactionCurator.ts
+++ curators/ReactionCurator.ts
@@ -4,6 +4,9 @@ import { ReactionTracker } from "@/reaction-tracker";
 import { ReactionDiscrepancyResolver } from "./ReactionDiscrepancyResolver";
 import { ensureFullMessage, shouldIgnoreMessage } from "@/handlers/util";
 import { determineContentType } from "./utils";
+import { getPostOrOddjobReactions } from "@/data/reaction";
+import { logger } from "@/client";
+import { loggableDbEmoji } from "@/handlers/log-utils";
 
 export class ReactionCurator {
   static isResolvingDiscrepancies = false;
@@ -14,6 +17,7 @@ export class ReactionCurator {
   ): Promise<void> {
     // the index.ts fetches PartialMessages so this is safe
     const message = reaction.message as Message;
+    const messageLink = `https://discord.com/channels/${message.guild?.id}/${message.channel.id}/${message.id}`;
     const { contentType } = determineContentType(message);
 
     if (!contentType || shouldIgnoreMessage(message)) {
@@ -34,8 +38,15 @@ export class ReactionCurator {
       this.isResolvingDiscrepancies = false;
 
       if (hadDiscrepancies) {
-        console.log(
-          `discrepancies were handled for reaction ${reaction.emoji.name || reaction.emoji.id}, returning`,
+        const dbReactionsAfterResolution = (
+          await getPostOrOddjobReactions(message.id, contentType)
+        )
+          .map((r) => loggableDbEmoji(r.emoji))
+          .join(", ");
+
+        logger.log(
+          `[${contentType}] Discrepancies were handled successfully, the reactions state in the database is now in sync with Discord ${messageLink}:
+            ${dbReactionsAfterResolution}`,
         );
         // return as the ReactionDiscrepancyResolver will handle the reactions including the latest one
         return;
